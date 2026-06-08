d74cf33860b808b21276c9ff6d8006c656c27584
alharith alkindi <alharithalk>
2026-06-08 10:07:14 +0400

    start do the Evaluation

--- src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
+++ src/main/java/com/oraclequantapi/oraclequantapi/OraclequantapiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OraclequantapiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(OraclequantapiApplication.class, args);
 	}
